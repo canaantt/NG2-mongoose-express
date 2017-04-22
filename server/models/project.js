659646e194055b65f98637f82dff13964f41de08
canaantt <canaantt@gmail.com>
2017-04-22 11:46:36 -0700

    new mongoDB dump & by adding IRBNumber (need to refer those numbers from database irb collection), project can be created. So far still not able to add file objects to project

--- server/models/project.js
+++ server/models/project.js
@@ -1,12 +1,14 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var File = require("../models/file");
+var IRB = require("../models/irb");
 var status = ["uploaded", "upload started", "failed"];
 var projectSchema = new Schema({
     Name: String,
     Description: String,
     Annotations: [{key: String, value: String}], 
     Files: [{type: Schema.ObjectId, ref:File}],
+    IRB: {type: Schema.ObjectId, ref:IRB},
     Date: {type: Date, default: Date.now}
 });
 module.exports = mongoose.model("Project", projectSchema);

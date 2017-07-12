98a718ac7624469a048817d9bd1b6ce581ad7d9f
canaantt <canaantt@gmail.com>
2017-07-12 15:57:27 -0700

    OAuth Works!

--- src/app/service/login-service.service.ts
+++ src/app/service/login-service.service.ts
@@ -1,19 +1,49 @@
 import { Injectable } from '@angular/core';
 import * as hello from 'hellojs';
+
 @Injectable()
 export class LoginServiceService {
+  GOOGLE_CLIENT_ID = '1098022410981-p7n5ejjji8qlvdtff274pol54jo5i8ks.apps.googleusercontent.com';
 
-  constructor() { }
-  // initial() {
-  //   hello.on('auth.login', function(auth){
+  constructor() {
+    hello.init({
+      google: this.GOOGLE_CLIENT_ID
+    }, {
+      force: true,
+      redirect_uri: '/landing'});
 
-  //     // call user information, for the given network
-  //     hello( auth.network ).api( '/me' ).then( function(r){
+    hello.on('auth.login', function(auth){
+      hello('google').api('me').then( function(v){
+        console.log(v);
+      });
+      console.log(auth);
+      console.log('logged in');
+    });
+    hello.on('auth.logout', function(auth){
+      console.log(auth);
+      console.log('logged out');
+    });
+  }
 
-  //       labelEl.innerHTML = '<img src="'+ r.thumbnail +'" /> Hey ' + r.name
-
-  //     })
-  //   })
-  // }
-  
+  googleLogin(): any {
+    alert('Hello');
+    hello.login('google', {
+                 display: 'popup',
+                 response_type: 'token',
+                 scope: 'email',
+                 redirect_uri: '/landing'
+             }, function(){
+               console.log('logged in...');
+             });
+    // hello.login('google',function(response, e){
+    //   if (e) { console.log(e.error.message); }
+    // });
+  }
+  googleLogOut(): any {
+    hello.logout('google', function(response, e){
+      console.log('logged in');
+      console.log(response);
+      if (e) { console.log(e.error.message); }
+    });
+  }
 }

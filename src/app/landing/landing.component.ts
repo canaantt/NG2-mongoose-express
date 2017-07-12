b0cfacca90adbd73a41e12b7b3510fd00fb91418
canaantt <canaantt@gmail.com>
2017-07-11 21:10:57 -0700

    added helloJS google OAuth piece, need to make it a service

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as hello from 'hellojs';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  GOOGLE_CLIENT_ID = '1098022410981-p7n5ejjji8qlvdtff274pol54jo5i8ks.apps.googleusercontent.com';
 
   constructor() { }
-
   ngOnInit() {
+    hello.init({
+      // facebook: FACEBOOK_CLIENT_ID,
+      // windows: WINDOWS_CLIENT_ID,
+      google: this.GOOGLE_CLIENT_ID
+    }, {redirect_uri: '/consent'});
+    hello.on('auth.login', function(auth){
+      console.log(auth);
+      console.log("logged in");
+    });
+    hello.on('auth.logout', function(auth){
+      console.log(auth);
+      console.log("logged out");
+    })
   }
 
+  googleLogin(): void {
+    alert("Hello");
+    hello('google').login();
+  }
+  googleLogOut(): void{
+    hello('google').logout();
+  }
 }
---
+++ src/app/service/login-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import * as hello from 'hellojs';
+@Injectable()
+export class LoginServiceService {
+
+  constructor() { }
+  // initial() {
+  //   hello.on('auth.login', function(auth){
+
+  //     // call user information, for the given network
+  //     hello( auth.network ).api( '/me' ).then( function(r){
+
+  //       labelEl.innerHTML = '<img src="'+ r.thumbnail +'" /> Hey ' + r.name
+
+  //     })
+  //   })
+  // }
+  
+}

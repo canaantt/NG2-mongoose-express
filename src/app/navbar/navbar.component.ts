2883873e84eefadbebfa58afe44cadb275f1f51f
canaantt <canaantt@gmail.com>
2017-07-14 13:51:11 -0700

    working on user login state

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../service/login-service.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [LoginServiceService]
 })
 
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginServiceService) {
+   }
 
   ngOnInit() {
+  }
+
+  login(): any {
+    console.log('in nav component');
+    this.loginService.googleLogin();
+  }
+  logout(): any {
+    console.log('in nav component logout');
+    this.loginService.googleLogOut();
   }
 
 }

e5d3ffb7fd88adc6a94ea261286355815b0a7d47
canaantt <canaantt@gmail.com>
2017-07-17 16:35:05 -0700

    MZ worked out subject behavior

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,46 @@
 import { Component, Output, Input, EventEmitter, OnInit} from '@angular/core';
 import * as hello from 'hellojs';
-
+import { StateService } from '../service/state.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers:[ StateService ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   GOOGLE_CLIENT_ID = '1098022410981-p7n5ejjji8qlvdtff274pol54jo5i8ks.apps.googleusercontent.com';
   @Input() authenticated;
   @Output('loginAction') loginAction: EventEmitter<any> = new EventEmitter<any>();
   @Output('sendUserInfo') sendUserInfo: EventEmitter<any> = new EventEmitter<any>();
   @Input() user;
-  constructor() {
+  constructor(private stateService: StateService) {
     hello.init({
       google: this.GOOGLE_CLIENT_ID
     }, {
       force: true,
       redirect_uri: '/landing'});
+      hello.on('auth.login', this.authLogin.bind(this));
+      hello.on('auth.logout', function(auth){
+       console.log(auth);
+       console.log('logged out');
+       this.sendUserInfo.emit(null);
+     });
   }
-
+  ngOnInit(){
+    // this.stateService.authenticated
+    //     .subscribe(res => {
+    //       this.authenticated = res;
+    //       console.log("in Child OnInit");
+    //       console.log(this.authenticated);
+    //     });
+    this.stateService.user
+        .subscribe(res => {
+          this.user = res;
+          console.log("in Child OnInit");
+          console.log(this.user);
+        });
+  }
   googleLogin(): any {
-    alert('Hello');
     console.log('In login component', this.authenticated);
     hello.login('google', {
                  display: 'popup',
@@ -29,32 +48,27 @@ export class LoginComponent {
                  scope: 'email',
                  redirect_uri: '/landing'
               }, this.sendingData.bind(this, true));
-    this.authenticated = true;
   }
+
+
   googleLogOut(): any {
-    hello.logout('google', this.sendingData.bind(this, false));
+    hello.logout('google', {}, this.sendingData.bind(this, false));
   }
-  monitor(): any {
-    console.log('in child monitor');
-    const self = this;
-    hello.on('auth.login', function(auth){
-      hello('google').api('me').then( function(v){
-        console.log(v);
-        self.sendUserInfo.emit(v);
-      });
-     });
-     hello.on('auth.logout', function(auth){
-       console.log(auth);
-       console.log('logged out');
-       self.sendUserInfo.emit(null);
-     });
+
+
+  authLogin(auth){
+    hello('google').api('me').then( this.updateUserInfo.bind(this) );    
   }
+  
+  updateUserInfo(v){
+    console.log(this.stateService.user);
+    this.stateService.user.next(v);
+  }
+
   sendingData(v: boolean): void {
     console.log(v);
     console.log('in sending data');
     this.loginAction.emit(v);
-    this.loginAction.emit({'hello': 'world',
-                           'world': 'hello'});
   }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,39 +1,70 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
 import { LoginComponent } from '../login/login.component';
-
+import { StateService } from '../service/state.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers:[ StateService ]
 })
 
-export class NavbarComponent implements OnInit{
-   @ViewChild(LoginComponent) loginComponent: LoginComponent;
-//  status: any = {
-//           'authenticated': false,
-//           'user': null
-//         }
- authenticated = false;
- user:any;
- 
-  constructor() {}
-  ngOnInit() {
-    console.log(this.authenticated);
-  }
+export class NavbarComponent implements OnInit {
+  @ViewChild(LoginComponent) loginComponent: LoginComponent;
 
-  authCB() {
-    console.log('in authentiation call back function');
-    this.loginComponent.monitor();
+ authenticated = false;
+ user: any;
+
+  constructor(private stateService: StateService) {
+    this.stateService.authenticated
+        .subscribe(res => {
+          this.authenticated = res;
+          console.log('in Nav constructor');
+          console.log(this.authenticated);
+        });
+    this.stateService.user
+        .subscribe(res => {
+          alert("GOT USER");
+          console.log("........" + res)
+          
+          this.user = res;
+          // console.log('in Nav constructor');
+          // console.log(this.user);
+        });
   }
-  pLoginAction (event) {
-    console.log(event);
-    this.authenticated = event;
-    this.authCB();
+  // refresh() {
+  //   this.stateService.authenticated
+  //       .subscribe(res => {
+  //         this.authenticated = res;
+  //         console.log("in Nav OnInit");
+  //         console.log(this.authenticated);
+  //       });
+  //   this.stateService.user
+  //       .subscribe(res => {
+  //         this.user = res;
+  //         console.log("in Nav OnInit");
+  //         console.log(this.user);
+  //       });
+  // }
+  ngOnInit(){
+    
   }
-  getUserInfo (event) {
-    console.log(event);
-    this.user = event;
-  }
-}
+//   authCB() {
+//     console.log('in authentiation call back function');
+//     this.loginComponent.monitor();
+//     this.refresh();
+//   }
+//   pLoginAction (event) {
+//     console.log(event);
+//     // this.authenticated = event;
+//     this.stateService.authenticated.next(event);
+//     this.authCB();
+//     this.refresh();
+//   }
+//   getUserInfo (event) {
+//     console.log(event);
+//     this.stateService.user.next(event);
+//     this.refresh();
+//   }
+ }
 
 
